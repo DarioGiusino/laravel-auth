8c1c831a34a0a0d72b8a54128716920d9083c0c4
DarioGiusino <dario.giusino@gmail.com>
2023-03-08 17:52:30 +0100

    defined update method inc ontroller

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -74,7 +74,11 @@ class ProjectController extends Controller
      */
     public function update(Request $request, Project $project)
     {
-        //
+        $data = $request->all();
+
+        $project->update($data);
+
+        return to_route('admin.projects.show', $project->id);
     }
 
     /**
