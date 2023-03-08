b9025080386acff8bb0489ee97c40c6dd8f7ca60
DarioGiusino <dario.giusino@gmail.com>
2023-03-08 18:07:29 +0100

    converted delete link in form

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -24,7 +24,10 @@
             <td class="text-end">
               <a class="btn btn-sm btn-primary" href="{{ route('admin.projects.show', $project->id) }}">Open</a>
               <a class="btn btn-sm btn-warning" href="{{ route('admin.projects.edit', $project->id) }}">Edit</a>
-              <a class="btn btn-sm btn-danger" href="{{ route('admin.projects.destroy', $project->id) }}">Delete</a>
+              <form class="d-inline" action="{{ route('admin.projects.destroy', $project->id) }}" method="post">
+                @csrf @method('delete')
+                <button class="btn btn-sm btn-danger">Delete</button>
+              </form>
             </td>
           </tr>
         @empty
