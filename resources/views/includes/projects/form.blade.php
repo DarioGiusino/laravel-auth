05fb4fdb4b6c0278fac8b790c8a3914e7bdbe448
DarioGiusino <dario.giusino@gmail.com>
2023-03-08 19:23:01 +0100

    inserte custom class and message if error in input create/edit

--- resources/views/includes/projects/form.blade.php
+++ resources/views/includes/projects/form.blade.php
@@ -1,9 +1,9 @@
 @if ($project->exists)
-  <form action="{{ route('admin.projects.update', $project->id) }}" method="post" novalidate> {{-- TODO remove novalidate --}}
+  <form action="{{ route('admin.projects.update', $project->id) }}" method="post">
     {{-- * method helper --}}
     @method('PUT')
   @else
-    <form action="{{ route('admin.projects.store') }}" method="post" novalidate> {{-- TODO remove novalidate --}}
+    <form action="{{ route('admin.projects.store') }}" method="post">
 @endif
 {{-- ! cross-site request forgery --}}
 @csrf
@@ -13,9 +13,13 @@
   <div class="col-4">
     <div class="mb-3">
       <label for="title" class="form-label">Title</label>
-      <input type="text" class="form-control" id="title" name="title" value="{{ $project->title }}" required
-        maxlength="40">
-      <div class="form-text">Add a project title</div>
+      <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title"
+        value="{{ $project->title }}" required maxlength="40">
+      @error('title')
+        <div class="invalid-feedback">{{ $message }}</div>
+      @else
+        <div class="form-text">Add a project title</div>
+      @enderror
     </div>
   </div>
 
@@ -23,8 +27,13 @@
   <div class="col-4">
     <div class="mb-3">
       <label for="repo_link" class="form-label">Repository link</label>
-      <input type="url" class="form-control" id="repo_link" name="repo_link" value="{{ $project->repo_link }}">
-      <div class="form-text">Link to the project repository</div>
+      <input type="url" class="form-control @error('repo_link') is-invalid @enderror" id="repo_link"
+        name="repo_link" value="{{ $project->repo_link }}">
+      @error('repo_link')
+        <div class="invalid-feedback">{{ $message }}</div>
+      @else
+        <div class="form-text">Link to the project repository</div>
+      @enderror
     </div>
   </div>
 
@@ -32,8 +41,13 @@
   <div class="col-4">
     <div class="mb-3">
       <label for="image" class="form-label">Image</label>
-      <input type="url" class="form-control" id="image" name="image" value="{{ $project->image }}">
-      <div class="form-text">Insert a project image</div>
+      <input type="url" class="form-control @error('image') is-invalid @enderror" id="image" name="image"
+        value="{{ $project->image }}">
+      @error('image')
+        <div class="invalid-feedback">{{ $message }}</div>
+      @else
+        <div class="form-text">Insert a project image</div>
+      @enderror
     </div>
   </div>
 
@@ -41,7 +55,8 @@
   <div class="col">
     <div class="mb-3">
       <label for="description" class="form-label">Description</label>
-      <textarea class="form-control" name="description" id="description" rows="10" required>{{ $project->description }}</textarea>
+      <textarea class="form-control @error('description') is-invalid @enderror" name="description" id="description"
+        rows="10" required>{{ $project->description }}</textarea>
     </div>
   </div>
 </div>
