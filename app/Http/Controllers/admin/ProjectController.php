307a688082aa696d93633251a9a4295674cd8d1a
DarioGiusino <dario.giusino@gmail.com>
2023-03-08 18:30:34 +0100

    unified forms create+edit with fake entity in create method

--- app/Http/Controllers/admin/ProjectController.php
+++ app/Http/Controllers/admin/ProjectController.php
@@ -26,7 +26,8 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        return view('admin.projects.create');
+        $project = new Project;
+        return view('admin.projects.create', compact('project'));
     }
 
     /**
---
+++ resources/views/includes/projects/form.blade.php
@@ -0,0 +1,55 @@
+@if ($project->exists)
+  <form action="{{ route('admin.projects.update', $project->id) }}" method="post">
+    {{-- * method helper --}}
+    @method('PUT')
+  @else
+    <form action="{{ route('admin.projects.store') }}" method="post">
+@endif
+{{-- ! cross-site request forgery --}}
+@csrf
+
+<div class="row">
+  {{-- title --}}
+  <div class="col-4">
+    <div class="mb-3">
+      <label for="title" class="form-label">Title</label>
+      <input type="text" class="form-control" id="title" name="title">
+      <div class="form-text">Add a project title</div>
+    </div>
+  </div>
+
+  {{-- repo_link --}}
+  <div class="col-4">
+    <div class="mb-3">
+      <label for="repo_link" class="form-label">Repository link</label>
+      <input type="url" class="form-control" id="repo_link" name="repo_link">
+      <div class="form-text">Link to the project repository</div>
+    </div>
+  </div>
+
+  {{-- image --}}
+  <div class="col-4">
+    <div class="mb-3">
+      <label for="image" class="form-label">Image</label>
+      <input type="text" class="form-control" id="image" name="image">
+      <div class="form-text">Insert a project image</div>
+    </div>
+  </div>
+
+  {{-- description --}}
+  <div class="col">
+    <div class="mb-3">
+      <label for="description" class="form-label">Description</label>
+      <textarea class="form-control" name="description" id="description" rows="10"></textarea>
+    </div>
+  </div>
+</div>
+
+<hr>
+
+{{-- form buttons --}}
+<div class="text-end me-2">
+  <button class="btn btn-sm btn-success">Save</button>
+  <a class="btn btn-sm btn-secondary" href="{{ route('admin.projects.index') }}">Go back</a>
+</div>
+</form>
