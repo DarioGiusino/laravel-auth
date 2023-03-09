190b7c923e96930b938624eea2a6de3446ee76ee
DarioGiusino <dario.giusino@gmail.com>
2023-03-09 20:49:05 +0100

    fix js cascade

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,8 @@
 import "./bootstrap";
 import "~resources/scss/app.scss";
 import "./form/delete";
-import "./form/is-published";
 import "./toast"; //must be after form/delete but before img-preview
+import "./form/is-published";
 import "./form/img-preview";
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**"]);
