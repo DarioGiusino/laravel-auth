61923dd2dedecc2b95232c34d4f11fed003e5020
DarioGiusino <dario.giusino@gmail.com>
2023-03-09 18:57:37 +0100

    fixed toast on create/edit

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import "./bootstrap";
 import "~resources/scss/app.scss";
 import "./form/delete";
+import "./toast"; //must be after form/delete but before img-preview
 import "./form/img-preview";
-import "./toast"; //must be the last script!!
 import * as bootstrap from "bootstrap";
 import.meta.glob(["../img/**"]);
