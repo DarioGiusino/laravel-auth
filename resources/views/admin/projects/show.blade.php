b89224b5e24c06500b2bca5981b31c12d8ff3225
DarioGiusino <dario.giusino@gmail.com>
2023-03-09 19:43:25 +0100

    inserted a status field in show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -6,6 +6,7 @@
     <header>
       <h2 class="text-center my-4">{{ $project->title }}</h2>
     </header>
+    {{-- content row --}}
     <div class="row row-cols-2">
       <div class="col d-flex justify-content-center align-items-center">
         <img class="img-fluid"
@@ -15,12 +16,17 @@
       <div class="col p-3">{{ $project->description }}</div>
     </div>
     <hr>
-    <div class="d-flex align-items-center justify-content-between">
+    <div class="d-flex align-items-center justify-content-between mb-5">
       <form class="d-inline delete-form" action="{{ route('admin.projects.destroy', $project->id) }}" method="post"
         data-form="{{ $project->title }}">
         @csrf @method('delete')
         <button class="btn btn-sm btn-danger">Delete</button>
       </form>
+      {{-- status --}}
+      <div>
+        <strong>Status: </strong><span
+          class="text-{{ $project->is_published ? 'success' : 'danger' }}">{{ $project->is_published ? 'Online' : 'Draft' }}</span>
+      </div>
       <div>
         <a class="btn btn-sm btn-warning" href="{{ route('admin.projects.edit', $project->id) }}">Edit</a>
         <a class="btn btn-sm btn-secondary" href="{{ route('admin.projects.index') }}">Go back</a>
